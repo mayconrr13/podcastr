b24cd10f83f4d7e9c5dde089f4216ced1f903bfe
Maycon Reis Rosário <mayconrr13@gmail.com>
2021-04-22 09:55:29 -0300

    play/pause button function

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -3,11 +3,26 @@ import { usePlayer } from '../../hooks/usePlayer'
 import styles from './styles.module.scss'
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
+import { useEffect, useRef } from 'react'
 
 export const Player = () => {
-  const { episodeList, currentEpisodeIndex } = usePlayer()
+  const { episodeList, currentEpisodeIndex, isPlaying, togglePlay, setPlayingState } = usePlayer()
 
   const episode = episodeList[currentEpisodeIndex]
+
+  const audioRef = useRef<HTMLAudioElement | null>(null)
+
+  useEffect(() => {
+    if(!audioRef.current) {
+      return
+    }
+
+    if (isPlaying) {
+      audioRef.current.play()
+    } else {
+      audioRef.current.pause()
+    }
+  }, [isPlaying]);
 
   return (
     <div className={styles.playerContainer}>
@@ -45,6 +60,16 @@ export const Player = () => {
           <span>00:00</span>
         </div>
 
+        {episode && (
+          <audio 
+            src={episode.file.url} 
+            ref={audioRef}
+            autoPlay 
+            onPlay={() => setPlayingState(true)}
+            onPause={() => setPlayingState(false)}
+          />
+        )}
+
         <div className={styles.buttons}>
           <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar"/>
@@ -52,8 +77,12 @@ export const Player = () => {
           <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior"/>
           </button>
-          <button type="button" className={styles.playButton} disabled={!episode}>
-            <img src="/play.svg" alt="Tocar"/>
+          <button type="button" className={styles.playButton} disabled={!episode} onClick={togglePlay}>
+            {isPlaying ? (
+              <img src="/pause.svg" alt="Tocar"/>
+            ) : (
+              <img src="/play.svg" alt="Tocar"/>
+            )}
           </button>
           <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima"/>
--- src/hooks/usePlayer.tsx
+++ src/hooks/usePlayer.tsx
@@ -16,7 +16,10 @@ interface Episode {
 interface PlayerContextData {
   episodeList: Episode[];
   currentEpisodeIndex: number;
+  isPlaying: boolean;
   play: (episode: Episode) => void;
+  togglePlay: () => void;
+  setPlayingState: (state: boolean) => void;
 }
 
 const PlayerContext = createContext<PlayerContextData>({} as PlayerContextData)
@@ -24,17 +27,30 @@ const PlayerContext = createContext<PlayerContextData>({} as PlayerContextData)
 export function PlayerProvider({ children }: ProviderProps): JSX.Element {
   const [episodeList, setEpisodeList] = useState([])
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
 
   function play(episode: Episode) {
     setEpisodeList([episode])
     setCurrentEpisodeIndex(0)
+    setIsPlaying(true)
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying)
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state)
   }
 
   return (
     <PlayerContext.Provider value={{
       episodeList,
       currentEpisodeIndex,
+      isPlaying,
       play,
+      togglePlay,
+      setPlayingState
     }} >
       {children}
     </PlayerContext.Provider>
